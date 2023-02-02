29fa9c166bacda614e097d6f0a658c4fd61e6d90
nahuel-gallardo <gallardon60@mail.com>
2023-02-01 23:52:39 -0300

    tp1

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,62 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    let producto1;
+    let producto2;
+    let producto3;
+    let mensaje;
+
+    producto1 = document.getElementById("txtIdPrecioUno").value;
+    producto2 = document.getElementById("txtIdPrecioDos").value;
+    producto3 = document.getElementById("txtIdPrecioTres").value;
+
+    producto1 = parseInt(producto1);
+    producto2 = parseInt(producto2);
+    producto3 = parseInt(producto3);
+
+    mensaje = producto1 + producto2 + producto3;
+
+    alert(mensaje);
+
 }
 function Promedio () 
 {
-	
+	let producto1;
+    let producto2;
+    let producto3;
+    let mensaje;
+
+    producto1 = document.getElementById("txtIdPrecioUno").value;
+    producto2 = document.getElementById("txtIdPrecioDos").value;
+    producto3 = document.getElementById("txtIdPrecioTres").value;
+
+    producto1 = parseInt(producto1);
+    producto2 = parseInt(producto2);
+    producto3 = parseInt(producto3);
+
+    mensaje = (producto1 + producto2 + producto3) / 3;
+
+    alert(mensaje);
+
+
 }
 function PrecioFinal () 
 {
+    let producto1;
+    let producto2;
+    let producto3;
+    let mensaje;
+
+    producto1 = document.getElementById("txtIdPrecioUno").value;
+    producto2 = document.getElementById("txtIdPrecioDos").value;
+    producto3 = document.getElementById("txtIdPrecioTres").value;
+
+    producto1 = parseInt(producto1);
+    producto2 = parseInt(producto2);
+    producto3 = parseInt(producto3);
+
+    mensaje = (producto1 + producto2 + producto3) * 1.21;
+
+    alert(mensaje);
 	
 }
