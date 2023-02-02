4723bd9859d85004700c12ac207906ec4b6557bb
nahuel-gallardo <gallardon60@mail.com>
2023-02-02 00:41:24 -0300

    tp3

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,37 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+	let fahrenheit;
+    let centígrados;
+    let mensaje;
+
+    fahrenheit = document.getElementById("txtIdTemperatura").value;
+
+    fahrenheit = parseInt(fahrenheit);
+
+    centígrados = (fahrenheit - 32) / 1.8;
+
+    mensaje = fahrenheit + " Fahrenheit son " + centígrados + " centígrados" ;
+
+    alert(mensaje);
+
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	let fahrenheit;
+    let centígrados;
+    let mensaje;
+
+    centígrados  = document.getElementById("txtIdTemperatura").value;
+
+    centígrados = parseInt(centígrados);
+
+    fahrenheit = (centígrados * 1.8) + 32;
+
+    mensaje = centígrados + " centígrados son " + fahrenheit + "Fahrenheit";
+    
+    alert(mensaje);
+
+    
 }
