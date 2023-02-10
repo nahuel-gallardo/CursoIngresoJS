4347fc78108339a617989628f7f9c51575a3d8aa
nahuel-gallardo <gallardon60@mail.com>
2023-02-10 01:16:37 -0300

    sw

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -4,10 +4,81 @@ function mostrar()
 	let localidad;
 	let precioBase;
 	let porcentaje;
+	let precioFinal;
+	let mensaje;
 
+	precioBase      = 15000;
 	estacionDelAnio = document.getElementById("txtIdEstacion").value;
 	localidad       = document.getElementById("txtIdDestino").value;
 
-	switch
+	switch(estacionDelAnio)
+	{
+		case "Invierno":
+			switch(localidad)
+			{
+				case "Bariloche":
+					porcentaje = 20;
+					break;
+				case "Cordoba":
+					porcentaje = -10;
+					break;
+				case "Cataratas":
+					porcentaje = -10;
+					break;
+				case "Mar del plata":
+					porcentaje = -20;
+					break;
+			}
+			break;
+
+		case "Verano":
+			switch(localidad)
+			{
+				case "Bariloche":
+					porcentaje = -20;
+					break;
+				case "Cordoba":
+					porcentaje = 10;
+					break;
+				case "Cataratas":
+					porcentaje = 10;
+					break;
+				case "Mar del plata":
+					porcentaje = 20;
+					break;
+			}
+			break;
+		
+		case "Otoño":
+		case "Primavera":
+			switch(localidad)
+			{
+				case "Bariloche":
+					porcentaje = 10;
+					break;
+				case "Cordoba":
+					porcentaje = 0;
+					break;
+				case "Cataratas":
+					porcentaje = 10;
+					break;
+				case "Mar del plata":
+					porcentaje = 10;
+					break;
+			}
+			break;
+	}
+
+	porcentaje = 1 + (porcentaje/100);
+
+	precioFinal = precioBase * porcentaje;
+
+	mensaje = precioFinal;
+
+	alert(mensaje);
+
+
+
+
 
 }//FIN DE LA FUNCIÓN
