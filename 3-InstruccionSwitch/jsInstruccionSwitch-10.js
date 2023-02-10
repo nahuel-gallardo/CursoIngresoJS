98ac33670a25863970f1d068ae8b83eaa2f85e28
nahuel-gallardo <gallardon60@mail.com>
2023-02-10 20:41:34 -0300

    switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,60 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacionDelAnio;
+	let localidad;
+	let mensaje;
 
+	estacionDelAnio = document.getElementById("txtIdEstacion").value;
+	localidad       = document.getElementById("txtIdDestino").value;
+
+	switch(estacionDelAnio)
+	{
+		case "Invierno":
+			switch(localidad)
+			{
+				case "Bariloche":
+					mensaje = "Se viaja";
+					break;
+				default:
+					mensaje = "No se viaja";
+					break;
+	        }
+			break;
+
+		case "Verano":
+			switch(localidad)
+			{
+				case "Mar del plata":
+				case "Cataratas":
+					mensaje = "Se viaja";
+					break;
+				default:
+					mensaje = "No se viaja";
+					break;
+			}
+			break;	
+
+		case "Otoño":
+			switch(localidad)
+			{
+				default:
+					mensaje = "Se viaja";
+					break;
+			}
+			break;
+			
+		case "Primavera":
+			switch(localidad)
+			{
+				case "Bariloche":
+					mensaje = "No se viaja";
+					break;
+				default:
+					mensaje = "Se viaja";
+					break;
+			}
+			break;					
+	}
+	
+	alert(mensaje);
 }//FIN DE LA FUNCIÓN
